d37b1c32819d9737e26df5de53c90ae73df3332a
SBasu007 <soumyadeep.basu19@gmail.com>
2025-02-17 08:13:02 +0530

    Changed tech stack

---
+++ Server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { createClient } from '@supabase/supabase-js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests from React frontend
+  }));
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
